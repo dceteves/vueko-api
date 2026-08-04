82bbd6d8904dcbb3ddff33291a4e56aa62f2e605
dceteves <dceteves@gmail.com>
2026-08-04 11:30:16 -0700

    Remove scripts

--- scripts/login.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-[ -f "cookies" ] && ./logout.sh
-
-curl -c cookies "localhost:3000/auth/login-test" &>/dev/null || echo "Error occurred"
--- scripts/team.sh
+++
@@ -1,7 +0,0 @@
-
-if [ ! -f "cookies" ]; then 
-    echo "Cookies file not found"
-    exit 1
-fi
-
-curl --request POST --header "Content-Type: application/json" --data '{ "name": "test" }' --cookie cookies  "http://localhost:3000/api/teams" 
